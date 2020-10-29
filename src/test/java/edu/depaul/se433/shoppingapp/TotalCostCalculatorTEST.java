a4f8890235386eea90108605512ce5cadcc1cf88
Piotr Morawiec <piotrmorawiec4@gmail.com>
2020-10-29 14:11:43 -0500

    Added CSV test files

--- src/test/java/edu/depaul/se433/shoppingapp/TotalCostCalculatorTEST.java
+++ src/test/java/edu/depaul/se433/shoppingapp/TotalCostCalculatorTEST.java
@@ -1,13 +1,40 @@
 package edu.depaul.se433.shoppingapp;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TotalCostCalculatorTEST {
-    @Test
-    @DisplayName("yooooooooo")
-    void yo(){
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/WeakNormal.csv")
+    @DisplayName("weakNormal")
+    void weakNormal(int initialCost,String State,ShippingType shipping, double expect ){
+        assertEquals(initialCost, 55);
     }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/WeakRobust.csv")
+    @DisplayName("weakRobust")
+    void weakRobust(int initialCost,String State,ShippingType shipping, double expect ){
+        assertEquals(initialCost, 55);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/StrongNormal.csv")
+    @DisplayName("StrongNormal")
+    void strongNormal(int initialCost,String State,ShippingType shipping, double expect ){
+        assertEquals(initialCost, 55);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/StrongRobust.csv")
+    @DisplayName("StrongRobust")
+    void strongRobust(int initialCost,String State,ShippingType shipping, double expect ){
+        assertEquals(initialCost, 55);
+    }
+
 }
